6f1c99db108943755e51ed65f35afb3412870612
hannafreitasc <hannafreitascc@gmail.com>
2025-04-25 11:47:58 -0300

    inicio

---
+++ src/components/FrontCard/index.jsx
@@ -0,0 +1,31 @@
+import { FcSimCardChip } from "react-icons/fc";
+import { LuNfc } from "react-icons/lu";
+
+export default function CardFront(){
+    return(
+        <div className="w-[500px] h-[350px] bg-black rounded-xl">
+            <div className="w-full h-[30%] flex">
+                <div className="w-[50%] h-full flex items-center pl-5 gap-2">
+                    <div className="w-[70px] h-[70px] rounded-full bg-white"></div>
+                    <div className="w-[50px] h-[50px] rounded-full bg-white"></div>
+                </div>
+                <div className="w-[50%] h-full flex p-4 justify-end">
+                    <p className="text-[20px] text-white">Nome do Banco</p>
+                </div>
+            </div>
+            <div className="w-full h-[40%] flex flex-col">
+                <div className="w-full h-[60%] flex items-center pl-2">
+                    <FcSimCardChip size={70}/> 
+                    <LuNfc size={60} color="#fff" />
+                </div>
+                <div className="w-full h-[40%] pl-4">
+                    <p className="text-[30px] text-gray-500">0000 0000 0000 0000</p>
+                </div>
+            </div>
+            <div className="w-full h-[30%] pl-4">
+                <p className="text-white text-[35px]">Nome no cartão</p>
+            </div>
+
+        </div>
+    )
+}
